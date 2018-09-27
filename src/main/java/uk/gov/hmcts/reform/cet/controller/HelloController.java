2fd4bddaa9c8c94debfd06c19cd976ec6f7106f1
johnhosolirius <john.ho@solirius.com>
2018-09-27 15:38:31 +0100

    1st cut of hello world with tests

---
+++ src/main/java/uk/gov/hmcts/reform/cet/controller/HelloController.java
@@ -0,0 +1,14 @@
+package uk.gov.hmcts.reform.cet.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+    
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Civil Enforcement!";
+    }
+    
+}
