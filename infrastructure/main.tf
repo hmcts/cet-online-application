6f736ef3ec5dc166190d19cbe01edafc71e512a3
John Ho <john.ho@solirius.com>
2018-10-04 16:19:56 +0100

    Infra

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -79,7 +79,8 @@ module "db" {
 
 data "azurerm_key_vault" "cet_key_vault" {
   name = "${local.vaultName}"
-  resource_group_name = "${local.vaultName}"
+  resource_group_name = "${module.app.resource_group_name}"
+  //  resource_group_name = "${local.vaultName}"
 //  resource_group_name = "${azurerm_resource_group.rg.name}"
 }
 
