f706913a7a10820706e26df08224d544fb1de5f0
johnhosolirius <john.ho@solirius.com>
2018-11-01 14:06:46 +0000

    Enable security profile for testing

--- src/main/java/uk/gov/hmcts/reform/cet/config/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/cet/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.cet.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,6 +11,7 @@ import org.springframework.security.web.authentication.preauth.AbstractPreAuthen
 
 @Configuration
 @EnableWebSecurity
+@Profile("!functional")
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private final AbstractPreAuthenticatedProcessingFilter filter;
