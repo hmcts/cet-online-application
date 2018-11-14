e7ce1169fd7693e05eadf8609ca53b9b8fe445e0
johnhosolirius <john.ho@solirius.com>
2018-11-14 15:27:36 +0000

    Try something to make this work again

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -70,7 +70,7 @@ module "app" {
     JSON_CONSOLE_PRETTY_PRINT = "${var.json_console_pretty_print}"
     LOG_OUTPUT = "${var.log_output}"
 
-    FORCE_CHANGE = "delete me"
+//    FORCE_CHANGE = "delete me"
   }
 }
 
