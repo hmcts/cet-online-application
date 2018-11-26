093590a2317bda81c2fbd8282b5489eb2689c9de
taleb <taleb.benouaer@hmcts.net>
2018-11-26 11:15:35 +0000

    Correct key-vault url for gov-notify-api-key secret

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -23,6 +23,10 @@ output "s2s_url" {
   value = "http://${var.s2s_url}-${local.local_env}.service.core-compute-${local.local_env}.internal"
 }
 
+output "gov_notify_api_key" {
+  value = "${module.app.gov_notify_api_key}"
+}
+
 output "spring_datasource_url" {
   value = "jdbc:postgresql://${module.db.host_name}:${module.db.postgresql_listen_port}/${module.db.postgresql_database}?ssl=true"
 }
