110b11777d949b023cc35fd181fe05eaa2c65ac6
johnhosolirius <john.ho@solirius.com>
2018-11-26 17:43:35 +0000

    1st Attempt at Talking to Fee Service

---
+++ src/aat/java/uk/gov/hmcts/reform/cet/functional/FeeServiceTest.java
@@ -0,0 +1,39 @@
+package uk.gov.hmcts.reform.cet.functional;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+import uk.gov.hmcts.reform.cet.model.Fee;
+
+import java.net.URI;
+
+import static org.junit.Assert.assertEquals;
+
+@SpringBootTest
+@ContextConfiguration
+public class FeeServiceTest {
+
+    private String feeApiUrl = "http://localhost:4411/fees-register/fees/lookup";
+
+    private RestTemplate restTemplate = new RestTemplate();
+
+    @Test
+    public void getFee() {
+
+        URI uri = UriComponentsBuilder.fromHttpUrl(feeApiUrl)
+                .queryParam("channel", "default")
+                .queryParam("event", "miscellaneous")
+                .queryParam("jurisdiction1", "civil")
+                .queryParam("jurisdiction2", "high court")
+                .queryParam("service", "civil money claims")
+                .build().encode().toUri();
+
+        ResponseEntity<Fee> feeResponse = restTemplate.getForEntity(uri, Fee.class);
+        Fee fee = feeResponse.getBody();
+        assertEquals("FEE0011", fee.getCode());
+        assertEquals("Appellant’s/respondent’s notice (High Court)", fee.getDescription());
+    }
+}
