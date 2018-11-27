0965de2e490a0971f606ac315809103aed905234
johnhosolirius <john.ho@solirius.com>
2018-11-27 10:45:31 +0000

    Refactor to use better configuration for integration test instead of functional

--- src/main/java/uk/gov/hmcts/reform/cet/service/FeePaymentServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/cet/service/FeePaymentServiceImpl.java
@@ -1,27 +1,29 @@
-package uk.gov.hmcts.reform.cet.functional;
+package uk.gov.hmcts.reform.cet.service;
 
-import org.junit.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import uk.gov.hmcts.reform.cet.model.Fee;
 
 import java.net.URI;
 
-import static org.junit.Assert.assertEquals;
+@Service
+@Slf4j
+public class FeePaymentServiceImpl implements FeePaymentService {
 
-@SpringBootTest
-@ContextConfiguration
-public class FeeServiceTest {
 
     private String feeApiUrl = "http://localhost:4411/fees-register/fees/lookup";
 
-    private RestTemplate restTemplate = new RestTemplate();
+    @Autowired
+    private RestTemplate restTemplate;
 
-    @Test
-    public void getFee() {
+    @Override
+    public Fee getFee(String event) {
+
+        log.debug("Getting fee from: " + feeApiUrl);
 
         URI uri = UriComponentsBuilder.fromHttpUrl(feeApiUrl)
                 .queryParam("channel", "default")
@@ -32,8 +34,7 @@ public class FeeServiceTest {
                 .build().encode().toUri();
 
         ResponseEntity<Fee> feeResponse = restTemplate.getForEntity(uri, Fee.class);
-        Fee fee = feeResponse.getBody();
-        assertEquals("FEE0011", fee.getCode());
-        assertEquals("Appellant’s/respondent’s notice (High Court)", fee.getDescription());
+
+        return feeResponse.getBody();
     }
 }
