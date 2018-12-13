2bd4b00b1ded4084f01e371ce82c4018f5e770aa
johnhosolirius <john.ho@solirius.com>
2018-12-13 10:10:35 +0000

    Revert HelloController changed by mistake

--- src/main/java/uk/gov/hmcts/reform/cet/controller/HelloController.java
+++ src/main/java/uk/gov/hmcts/reform/cet/controller/HelloController.java
@@ -1,28 +1,15 @@
 package uk.gov.hmcts.reform.cet.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import uk.gov.hmcts.reform.cet.model.Fee;
-import uk.gov.hmcts.reform.cet.service.FeePaymentService;
-import uk.gov.hmcts.reform.cet.service.FeePaymentServiceClient;
 
 @RestController
 @RequestMapping("/civil-enforcement")
 public class HelloController {
 
-    @Autowired
-    FeePaymentServiceClient client;
-
     @RequestMapping("")
-    public Fee greet() {
-
-        return client.getFee("civil money claims",
-                "civil",
-                "high court",
-                "default",
-                "miscellaneous"
-                );
+    public String greet() {
+        return "Greetings from Civil Enforcement!";
     }
-    
+
 }
